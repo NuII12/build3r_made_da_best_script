9b17c4802e28fe4e97ed6811b028278df8192c8c
build3r <yanis.mag4801@gmail.com>
2026-05-03 22:50:13 +0200

    Add additional burger buttons and teleport functionality

--- script.lua
+++ script.lua
@@ -20,6 +20,9 @@ local Lists = Instance.new("Frame")
 local UIListLayout = Instance.new("UIListLayout")
 local Burger1 = Instance.new("TextButton")
 local Burger2 = Instance.new("TextButton")
+local Burger3 = Instance.new("TextButton")
+local Burger4 = Instance.new("TextButton")
+local Burger5 = Instance.new("TextButton")
 
 buil3rGui.Name = "build3rGui"
 buil3rGui.Parent = playerGui
@@ -94,6 +97,50 @@ Burger2.TextSize = 14.000
 Burger2.TextStrokeTransparency = 0.000
 Burger2.TextWrapped = true
 
+Burger3.Name = "Burger3"
+Burger3.Parent = Lists
+Burger3.BackgroundColor3 = Color3.fromRGB(213, 255, 207)
+Burger3.BorderColor3 = Color3.fromRGB(0, 0, 0)
+Burger3.BorderSizePixel = 3
+Burger3.Position = UDim2.new(0.0700000003, 0, 0.0700000003, 0)
+Burger3.Size = UDim2.new(0.850000024, 0, 0.100000001, 0)
+Burger3.Font = Enum.Font.FredokaOne
+Burger3.Text = "World 3"
+Burger3.TextColor3 = Color3.fromRGB(255, 255, 255)
+Burger3.TextScaled = true
+Burger3.TextSize = 14.000
+Burger3.TextStrokeTransparency = 0.000
+Burger3.TextWrapped = true
+
+Burger4.Name = "Burger4"
+Burger4.Parent = Lists
+Burger4.BackgroundColor3 = Color3.fromRGB(213, 255, 207)
+Burger4.BorderColor3 = Color3.fromRGB(0, 0, 0)
+Burger4.BorderSizePixel = 3
+Burger4.Position = UDim2.new(0.0700000003, 0, 0.0700000003, 0)
+Burger4.Size = UDim2.new(0.850000024, 0, 0.100000001, 0)
+Burger4.Font = Enum.Font.FredokaOne
+Burger4.Text = "World 4"
+Burger4.TextColor3 = Color3.fromRGB(255, 255, 255)
+Burger4.TextScaled = true
+Burger4.TextSize = 14.000
+Burger4.TextStrokeTransparency = 0.000
+Burger4.TextWrapped = true
+
+Burger5.Name = "Burger5"
+Burger3.Parent = Lists
+Burger5.BackgroundColor3 = Color3.fromRGB(213, 255, 207)
+Burger5.BorderColor3 = Color3.fromRGB(0, 0, 0)
+Burger5.BorderSizePixel = 3
+Burger5.Position = UDim2.new(0.0700000003, 0, 0.0700000003, 0)
+Burger5.Size = UDim2.new(0.850000024, 0, 0.100000001, 0)
+Burger5.Font = Enum.Font.FredokaOne
+Burger5.Text = "World 5"
+Burger5.TextColor3 = Color3.fromRGB(255, 255, 255)
+Burger5.TextScaled = true
+Burger5.TextSize = 14.000
+Burger5.TextStrokeTransparency = 0.000
+Burger5.TextWrapped = true
 
 local userId = player.UserId
 local thumbType = Enum.ThumbnailType.HeadShot
@@ -132,6 +179,21 @@ local burger2 = game.Workspace:WaitForChild("Maps")
 	:WaitForChild("Model")
 	:WaitForChild("burger")
 
+local burger3 = game.Workspace:WaitForChild("Maps")
+	:WaitForChild("Map3")
+	:WaitForChild("Model")
+	:WaitForChild("burger")
+
+local burger4 = game.Workspace:WaitForChild("Maps")
+	:WaitForChild("Map4")
+	:WaitForChild("Model")
+	:WaitForChild("burger")
+
+local burger5 = game.Workspace:WaitForChild("Maps")
+	:WaitForChild("Map5")
+	:WaitForChild("Model")
+	:WaitForChild("burger")
+
 local function teleportTo(part)
 	local character = player.Character or player.CharacterAdded:Wait()
 	local hrp = character:WaitForChild("HumanoidRootPart")
@@ -144,4 +206,16 @@ end)
 
 Burger2.MouseButton1Click:Connect(function()
 	teleportTo(burger2)
+end)
+
+Burger3.MouseButton1Click:Connect(function()
+	teleportTo(burger3)
+end)
+
+Burger4.MouseButton1Click:Connect(function()
+	teleportTo(burger4)
+end)
+
+Burger5.MouseButton1Click:Connect(function()
+	teleportTo(burger5)
 end)
